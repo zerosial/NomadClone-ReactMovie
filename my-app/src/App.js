168fd40eeb2b8fa458f1f36030b064f8688a7958
zerosial <zerosial@gmail.com>
2022-05-30 05:31:46 +0900

    📰New: Todolist , coin api 불러오기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,68 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {}
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
 
-export default App;
+  return (
+    <div>
+      <h1>The Coins! {coins.length}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((item, index) => (
+            <option key={index}>
+              {item.name} ({item.symbol}: $ {item.quotes.USD.price})
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+}
+
+function MyToDos() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, settoDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    settoDos((currentArray) => [toDo, ...currentArray]);
+  };
+  return (
+    <div>
+      <h1>My To Dos</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export { App, MyToDos };
